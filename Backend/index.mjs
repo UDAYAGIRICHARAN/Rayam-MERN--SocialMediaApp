1ae7d6884ea7ca163e5de48c02d8062baebad658
udayagiricharan <udayagiricharankumar@gmail.com>
2023-06-24 19:07:47 +0530

    updated the cofig

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -15,7 +15,7 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
-import {register} from "./controllers/auth.js";
+
 
 /* config */
 const __filename = fileURLToPath(import.meta.url);
